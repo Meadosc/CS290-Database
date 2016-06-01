312fc9ce9a0cace6d3f2f89103fdbcc84cd80966
Meadosc <meadosc@onid.orst.edu>
2016-06-01 16:26:55 -0700

    Update clientScript.js

--- public/clientScript.js
+++ public/clientScript.js
@@ -1,23 +1,18 @@
 document.addEventListener('DOMContentLoaded', callSelect);
 
 function callSelect(){
-	console.log("it makes it inside the function"); //testing
 	var req = new XMLHttpRequest();
 	req.open('POST', "http://52.36.135.10:3000/select", true);
 	req.setRequestHeader('Content-Type', 'application/json');
 	req.addEventListener('load',function(){
-		console.log("it makes it inside the AJAX 'load' event listener"); //testing
-		/*if(req.status >= 200 && req.status < 400){*/
-			console.log("Just before the assignment of the JSON response"); //testing
-			console.log("req.responseText: " + req.responseText); //testing
-			
-			var response = JSON.parse(req.responseText); // This gives us the response as a variable
-			
-			console.log("Just after the assignment of the JSON response"); //testing
-			console.log(response); //testing to see if I get the JSON back. duplicat below
-		/*} else {
-			console.log("Error in network request: " + req.statusText);
-		}*/
+		console.log("Just before the assignment of the JSON response"); //testing
+		console.log("req: " + req); //testing
+		console.log("req.responseText: " + req.responseText); //testing
+		
+		var response = JSON.parse(req.responseText); // This gives us the response as a variable
+		
+		console.log("Just after the assignment of the JSON response"); //testing
+		console.log(response); //testing to see if I get the JSON back. duplicat below
 	});
 	req.send(JSON.stringify()); //Send the content
 			
