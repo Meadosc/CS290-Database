74c0925af52896426242fc40b01f661f68667659
Meadosc <meadosc@onid.orst.edu>
2016-06-01 16:33:23 -0700

    Update clientScript.js

--- public/clientScript.js
+++ public/clientScript.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', callSelect);
 
 function callSelect(){
 	var req = new XMLHttpRequest();
-	req.open('POST', "http://52.36.135.10:3000/select", true);
+	req.open('GET', "http://52.36.135.10:3000/select", true);
 	req.setRequestHeader('Content-Type', 'application/json');
 	req.addEventListener('load',function(){
 		console.log("Just before the assignment of the JSON response"); //testing
@@ -14,7 +14,7 @@ function callSelect(){
 		console.log("Just after the assignment of the JSON response"); //testing
 		console.log(response); //testing to see if I get the JSON back. duplicat below
 	});
-	req.send(JSON.stringify()); //Send the content
+	req.send(); //Send the content
 			
 	// I'm not clicking anything. event.preventDefault(); //Stops form from refreshing page
 };
