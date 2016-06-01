e664cb86d5c23b58e0547e61d8639d4163f617df
Meadosc <meadosc@onid.orst.edu>
2016-06-01 16:39:44 -0700

    Update clientScript.js

--- public/clientScript.js
+++ public/clientScript.js
@@ -7,9 +7,25 @@ function callSelect(){
 	req.addEventListener('load',function(){
 		console.log("req.responseText: " + req.responseText); //testing
 		var response = JSON.parse(req.responseText); // This gives us the response as a variable
-		
+		createTable(response); //Creates the table
 	});
 	req.send(); //Send the content
 			
 	// I'm not clicking anything. event.preventDefault(); //Stops form from refreshing page
 };
+
+//Creates a table. Written by Kyle Prouty
+function createTable(data){
+    var table = document.createElement("table");
+
+    for(var i in data){
+        var row = document.createElement("tr");        
+        for(var j in data[i]){
+            var col = document.createElement("td");
+            col.textContent = data[i][j];
+            row.appendChild(col);
+       	}
+        table.appendChild(row);
+    }
+    $('#table-wrap').append(table);
+}
