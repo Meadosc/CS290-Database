5f065144725dec6d6f0fb913acd723aa66c7ac44
Meadosc <meadosc@onid.orst.edu>
2016-06-01 16:36:14 -0700

    Update clientScript.js

--- public/clientScript.js
+++ public/clientScript.js
@@ -5,14 +5,9 @@ function callSelect(){
 	req.open('GET', "http://52.36.135.10:3000/select", true);
 	req.setRequestHeader('Content-Type', 'application/json');
 	req.addEventListener('load',function(){
-		console.log("Just before the assignment of the JSON response"); //testing
-		console.log("req: " + req); //testing
 		console.log("req.responseText: " + req.responseText); //testing
-		
 		var response = JSON.parse(req.responseText); // This gives us the response as a variable
 		
-		console.log("Just after the assignment of the JSON response"); //testing
-		console.log(response); //testing to see if I get the JSON back. duplicat below
 	});
 	req.send(); //Send the content
 			
