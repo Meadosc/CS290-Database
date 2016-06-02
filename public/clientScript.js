e032ee6f685baf85fd35c5fe94713b506ff69dcd
Meadosc <meadosc@onid.orst.edu>
2016-06-01 22:39:55 -0700

    Update clientScript.js

--- public/clientScript.js
+++ public/clientScript.js
@@ -1,43 +1,6 @@
 document.addEventListener('DOMContentLoaded', callSelect);//Renders page and calls to select page.
 document.addEventListener('DOMContentLoaded', bindInsertButton);//Binds the input button
 
-function bindInsertButton(){
-    document.getElementById("insertSubmit").addEventListener("click", function(event){
-	console.log("bindInsertButton is called with click event"); // debug
-	
-	var payload = {}; //payload is the object I will use to send data to the insert page
-	payload.name = document.getElementById('name').value;
-	payload.reps = document.getElementById('reps').value;
-	payload.weight = document.getElementById('weight').value;
-	payload.date = document.getElementById('date').value;
-	var radio = document.getElementsByName('units').value;
-	
-	if(radio[0].checked) {payload.units = "1"}
-	else {payload.units = "0"}
-    	
-    	var requestString = "name=" + payload.name + "&reps=" + payload.reps + "&weight=" + payload.weight + "&date=" payload.date + "&lbs=" + payload.units;
-    	
-        var req = new XMLHttpRequest();
-        req.open("POST", "http://52.36.135.10:3000/insert?" + requestString,true);
-        req.setRequestHeader("Content-Type", "application/json");
-    
-        req.addEventListener("load", function(){
-        
-            if(req.status >= 200 && req.status < 400){
-                var response = JSON.parse(req.responseText);
-                console.log(response.data);
-                console.log(response);
-                console.log(req);
-            }
-            else{
-                console.log("Error in network request: " + req.statusText);
-                console.log(req);
-            }
-        });
-        //event.preventDefault(); //stop page from refreshing
-        });
-};
-/*
 //calls to the input page and inserts data
 function bindInsertButton(){
 	//Listener for the insert button
@@ -69,7 +32,7 @@ function bindInsertButton(){
 		req.send(); //Send the content	
 	});
 };
-*/
+
 //calls the select page to get info for table
 function callSelect(){
 	var req = new XMLHttpRequest();
