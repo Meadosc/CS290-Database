e0842d0457d8eaece4adfc7989f0f8120467de93
Meadosc <meadosc@onid.orst.edu>
2016-06-01 17:22:09 -0700

    Update clientScript.js

--- public/clientScript.js
+++ public/clientScript.js
@@ -19,16 +19,7 @@ function createTable(data){
    
 	var table = document.createElement("table");//Create table in HTML
 	document.body.appendChild(table); //Add the table as a child of the body in HTML
-	
-	/*
-	//Add in table headers
-	var row = document.createElement("tr");  
-	table.appendChild(row); //Create a new row
-	var col = document.createElement("th");
-	col.textContent = "id"; //add ID header
-	row.appendChild(col);
-	*/
-	
+
 	//Create table headers
 	var headers = ["id","name","reps","weight","date","lbs"];
 	var row = document.createElement("tr"); 
