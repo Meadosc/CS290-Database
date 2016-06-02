cdf6a8ef80e986670a1c021cbe3db0ee4e0e9dc5
Meadosc <meadosc@onid.orst.edu>
2016-06-02 09:59:49 -0700

    Update serverData.js

--- serverData.js
+++ serverData.js
@@ -49,16 +49,11 @@ app.post('/insert',function(req, res, next){
 
 //Insert data into the database
 app.get('/insert',function(req,res,next){
-  var context = {};
   mysql.pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?,?,?,?,?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
     if(err){
       next(err);
       return;
     }
-    
-    context.results = "Inserted id " + result.insertId;
-    res.render('home',context);
-    
   });
 });
 
